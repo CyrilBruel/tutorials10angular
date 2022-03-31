336890eff3f3d810953ebe4e756141b57504f346
Bruel Cyril <razafiarison.bruel@gmail.com>
2022-03-31 17:27:01 +0300

    Update tutorial.service.ts

--- frontend/src/app/services/tutorial.service.ts
+++ frontend/src/app/services/tutorial.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const baseUrl = 'http://localhost:3000/api/tutorials';
+const baseUrl = 'https://tutorials10angular.herokuapp.com/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
